9ee3a6a8be1d35f611c3c400068bbad82bdbe24b
dinesh <113573757+dineshselvaraj11@users.noreply.github.com>
2022-10-14 11:07:49 +0530

    Add files via upload

---
+++ dellLaptop.java
@@ -0,0 +1,16 @@
+class DellLaptop {
+String KeyLang;
+boolean KeyLight;
+boolean is TouchDisplay;
+
+//public DellLoptop(StringKL,boolean KLight,boolean isTouchD){
+/*KeyLang=kL;
+Keylight=KLight;
+is TouchDisplay;
+}*/
+
+
+void watchingmovies(){
+System.out.println("watching movies");
+}
+}
